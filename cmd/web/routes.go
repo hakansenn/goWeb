68e0e09fda2fc64b098f7781ca066deecf8fe27d
Hakan SEN <hakan@hakansen.com>
2022-12-26 00:21:11 +0300

    26

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/hakansenn/goWeb/pkg/config"
 	"github.com/hakansenn/goWeb/pkg/handlers"
-	"net/http"
 )
 
 func routes(app *config.AppConfig) http.Handler {
@@ -13,7 +14,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-	mux.Use(Deneme)
+	mux.Use(WriteToConsole)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
