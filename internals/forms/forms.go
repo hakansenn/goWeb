1379769e263a3d82145930fd228f19f5f9cd5eb0
Hakan SEN <115928237+hakansenn@users.noreply.github.com>
2023-01-04 15:59:09 +0300

    server side validation

--- internals/forms/forms.go
+++ internals/forms/forms.go
@@ -5,7 +5,7 @@ import (
 	"net/url"
 )
 
-// Form creates a custom form struct, embeds a urlValues object
+// Form creates a custom form struct, embeds a url.Values object
 type Form struct {
 	url.Values
 	Errors errors
@@ -24,9 +24,16 @@ func (f *Form) Has(field string, r *http.Request) bool {
 
 	x := r.Form.Get(field)
 	if x == "" {
+		f.Errors.Add(field,"This field cannot be blank")
 		return false
 	}
 
 	return true
 
+}
+
+
+//Valid return true if there are no errors, otherwise false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
 }
