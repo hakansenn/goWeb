b0ce67f9da6647975b5cd2f8c1870171d3010105
Hakan SEN <hakan@hakansen.com>
2023-01-25 23:23:52 +0300

    added tests for handlers

--- internals/handlers/setup_test.go
+++ internals/handlers/setup_test.go
@@ -2,8 +2,11 @@ package handlers
 
 import (
 	"encoding/gob"
+	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
+	"html/template"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
@@ -16,35 +19,36 @@ import (
 
 var app config.AppConfig
 var session *scs.SessionManager
+var pathToTemplates = "./../../templates"
+var functions = template.FuncMap{}
 
-func getRoutes () http.Handler{
-//what am i going to put in the session
-gob.Register(models.Reservation{})
+func getRoutes() http.Handler {
+	gob.Register(models.Reservation{})
 
-app.InProduction = false
+	app.InProduction = false
 
-session = scs.New()
-session.Lifetime = 24 * time.Hour
-session.Cookie.Persist = true
-session.Cookie.SameSite = http.SameSiteLaxMode
-session.Cookie.Secure = app.InProduction
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
 
-app.Session = session
+	app.Session = session
 
-tc, err := render.CreateTemplateCache()
-if err != nil {
-	log.Fatal("cannot create template cache")
-}
+	tc, err := CreateTestTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+	}
 
-app.TemplateCache = tc
-app.UseCache = false
+	app.TemplateCache = tc
+	app.UseCache = true
 
-repo := NewRepo(&app)
-NewHandlers(repo)
-render.NewTemplates(&app)
+	repo := NewRepo(&app)
+	NewHandlers(repo)
+	render.NewTemplates(&app)
 
-mux := chi.NewRouter()
-	mux.Use(NoSurf)
+	mux := chi.NewRouter()
+	//mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", Repo.Home)
@@ -55,7 +59,6 @@ mux := chi.NewRouter()
 	mux.Get("/search-availability", Repo.Availability)
 	mux.Post("/search-availability", Repo.PostAvailability)
 	mux.Post("/search-availability-json", Repo.AvailabilityJSON)
-	
 
 	mux.Get("/contact", Repo.Contact)
 
@@ -83,4 +86,37 @@ func NoSurf(next http.Handler) http.Handler {
 
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+func CreateTestTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", pathToTemplates))
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", pathToTemplates))
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+	}
+
+	return myCache, nil
 }
